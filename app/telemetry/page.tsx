d21457dd264bfd1ffdf629bf63cae0793fd3afce
loudsheep <loudsheep05@gmail.com>
2025-05-14 19:20:24 +0200

    feat: telemetry page

---
+++ app/telemetry/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Telemetry() {
+    return (
+        <div className="flex flex-col justify-center items-center mx-auto mt-10 text-2xl text-red-500">
+            Coming soon...
+        </div>
+    );
+}
