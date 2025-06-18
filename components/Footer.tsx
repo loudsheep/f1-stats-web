ad6f9ac72504636c2a789640d4d5056eb22a92f6
loudsheep <loudsheep05@gmail.com>
2025-06-18 16:49:43 +0200

    feat: add multiple contributorsr

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
+const contributors = [
+    { name: "loudsheep", url: "https://github.com/loudsheep" },
+    { name: "Sok205", url: "https://github.com/Sok205" }
+];
+
 export default function Footer() {
     return (
-        <div className='w-full text-center p-5'>
-            Made with ❤️ by <a href="https://github.com/loudsheep" className='underline' target='_blank'>loudsheep</a>
+        <div className='w-full text-center p-5 flex justify-center'>
+            Made with ❤️ by
+            <div className='flex gap-2 ml-2'>
+                {contributors.map((value, idx) => (
+                    <a href={value.url} className='underline' target='_blank'>{value.name}</a>
+                ))}
+            </div>
         </div>
     )
 }
