7d003e35c000ae770518ba000be4db593fd80853
loudsheep <loudsheep05@gmail.com>
2025-06-23 15:12:53 +0200

    fix: footer responsivness

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,9 +7,9 @@ const contributors = [
 
 export default function Footer() {
     return (
-        <div className='w-full text-center p-5 flex justify-center'>
+        <div className='w-full text-center p-5 flex justify-center flex-wrap'>
             Made with ❤️ by
-            <div className='flex gap-2 ml-2'>
+            <div className='flex gap-2 ml-2 flex-wrap justify-center'>
                 {contributors.map((value, idx) => (
                     <a href={value.url} className='underline' target='_blank'>{value.name}</a>
                 ))}
