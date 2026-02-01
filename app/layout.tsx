477a459ac39a2308987e4006c836f3beaeaf9321
loudsheep <loudsheep05@gmail.com>
2026-02-01 21:25:35 +0100

    feat: add analytics script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Script from "next/script";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -28,6 +29,14 @@ export default function RootLayout({
         <html lang="en">
             <head>
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=arrow_forward,open_in_new,pin_drop" />
+
+                <Script
+                    async={true}
+                    defer={true}
+                    src="https://analytics.loudsheep.dev/script.js"
+                    data-website-id="f119f855-6dd1-4bf9-8307-a397d9c2691e"
+                    data-domains="loudsheep.dev"
+                  />
             </head>
 
             <body
