60081729c38dfee424db5831f801c7bfbd6d9cbc
loudsheep <loudsheep05@gmail.com>
2026-02-01 21:32:17 +0100

    fix: update analytics domain to f1.loudsheep.dev

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
                     defer={true}
                     src="https://analytics.loudsheep.dev/script.js"
                     data-website-id="f119f855-6dd1-4bf9-8307-a397d9c2691e"
-                    data-domains="loudsheep.dev"
+                    data-domains="f1.loudsheep.dev"
                   />
             </head>
 
